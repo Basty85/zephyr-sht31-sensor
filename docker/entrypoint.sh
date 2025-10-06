44faa9d1e17cdb12c22874fe109c4e017b161bf2
Sebastian <41087043+Basty85@users.noreply.github.com>
2025-10-06 17:29:45 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,7 +13,6 @@ cd /workspace
 # Make scripts executable
 chmod +x /workspace/zephyr-sht31-sensor/temp_udp_app/scripts/*.sh
 
-# Falls ein Befehl übergeben wird, ausführen, sonst Bash
 if [ $# -eq 0 ]; then
     exec bash
 else
